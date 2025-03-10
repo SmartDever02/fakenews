c84111b82ad8852c7f733276a8e26e4dacdb1344
SmartDever02 <smart.jamesjin@gmail.com>
2025-03-09 19:16:55 -0700

    feat: remove unnecessary column

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -32,15 +32,6 @@ export async function POST(req: Request) {
       is_adjusted,
     } = parsedData.data
 
-    let is_valid_score_format = true
-
-    const p1 = predictions.at(0) || 0
-    const p2 = predictions.at(1) || 0
-
-    if ((p1 > 0.5 && p2 > 0.5) || (p1 <= 0.5 && p2 <= 0.5)) {
-      is_valid_score_format = false
-    }
-
     const newPayload = await prisma.logs.create({
       data: {
         article,
@@ -49,7 +40,6 @@ export async function POST(req: Request) {
         paraphrased_score,
         is_first_try_valid: is_valid_first_try,
         is_adjusted,
-        is_valid_score_format,
       },
     })
 

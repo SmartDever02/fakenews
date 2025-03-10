3892f5170513a93cff9fa553f31cc293709b3d9c
SmartDever02 <smart.jamesjin@gmail.com>
2025-03-09 21:12:22 -0700

    feat: temp ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,31 @@
-// import prisma from '@/lib/prisma'
+import prisma from '@/lib/prisma'
 
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
   const UIDArray = [234, 227, 43, 44, 42]
 
-  return <main className="p-20">{JSON.stringify(UIDArray)}</main>
+  let arr = []
+  for (let i = 0; i < UIDArray.length; i++) {
+    const data = await prisma.logs.findMany({
+      where: { uid: UIDArray[i] },
+      take: 250,
+    })
+    const count = data.filter(
+      (elem) => elem.is_first_try_valid === false
+    ).length
+    arr.push(count)
+  }
+
+  return (
+    <main className="p-20">
+      <ul>
+        {UIDArray.map((uid, index) => (
+          <li key={uid}>
+            UID: {uid} Invalid count: {arr[index]}
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
 }

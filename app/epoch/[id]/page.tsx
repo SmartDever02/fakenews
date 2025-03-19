ca5a5859dcf910912821097b5fbe9fa970e9c1ec
SmartDever02 <smart.jamesjin@gmail.com>
2025-03-19 12:39:29 -0700

    feat: disable page

--- app/epoch/[id]/page.tsx
+++ app/epoch/[id]/page.tsx
@@ -1,59 +1,9 @@
-import prisma from '@/lib/prisma'
-
 export default async function Page({ params }: { params: { id: number } }) {
-  const logsPerMiner = await prisma.logs.groupBy({
-    by: ['uid'],
-    where: { epoch_number: Number(params.id) },
-    _count: { id: true },
-    orderBy: { _count: { id: 'desc' } },
-  })
-
-  // Invalid logs per miner
-  const invalidLogsPerMiner = await prisma.logs.groupBy({
-    by: ['uid'],
-    where: {
-      epoch_number: Number(params.id),
-      OR: [{ is_first_try_valid: false }, { is_adjusted: true }],
-    },
-    _count: { id: true },
-    orderBy: { _count: { id: 'desc' } },
-  })
-
-  const skippedLogs = await prisma.skip_logs.groupBy({
-    by: ['uid'],
-    where: {
-      epoch_number: Number(params.id),
-    },
-    _count: { id: true },
-  })
-
   return (
     <main className="p-20">
       <h2 className="text-xl text-center font-semibold pb-20">
         Epoch {params.id}
       </h2>
-      {logsPerMiner.length === 0 && invalidLogsPerMiner.length === 0 ? (
-        <p className="text-center">No logs found for this epoch.</p>
-      ) : (
-        <>
-          <h3 className="text-xl text-center font-semibold pb-10">
-            Logs per miner
-          </h3>
-          <ul>
-            {logsPerMiner.map(({ uid, _count }) => (
-              <li key={uid}>
-                Miner: {uid}, Synapses: {_count.id} (Invalid Synapses:{' '}
-                {
-                  invalidLogsPerMiner.find((item) => item.uid === uid)?._count
-                    ?.id
-                }
-                , Skipped Synapses:{' '}
-                {skippedLogs.find((item) => item.uid === uid)?._count?.id || 0})
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
     </main>
   )
 }
